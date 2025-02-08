67389e517dc4d7917dc96d3dff883a6d7ab08ab2
Ahmed Osman <eng.ahmedosman00@gmail.com>
2025-02-08 13:24:35 +0100

    Create arrow formula

---
+++ arrow.rb
@@ -0,0 +1,11 @@
+class Arrow < Formula
+  desc "A powerful Swift-based command-line tool for generate dependacies file."
+  homepage "https://github.com/AhmedOsman00/arrow-script"
+  url "https://github.com/AhmedOsman00/repo/releases/download/tag/arrow"
+  sha256 "sha256"
+  version "version"
+
+  def install
+    bin.install "arrow"
+  end
+end
