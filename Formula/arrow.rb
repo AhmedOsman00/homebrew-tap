affae7a4ba20c2f7015074fde7675a0740b478fb
Ahmed Osman <eng.ahmedosman00@gmail.com>
2025-12-29 14:59:23 +0100

    Revise Arrow formula metadata and add test
    Updated description, homepage, and URL for the Arrow formula. Added a test block.

--- Formula/arrow.rb
+++ Formula/arrow.rb
@@ -1,11 +1,17 @@
 class Arrow < Formula
-  desc "A powerful Swift-based command-line tool for generate dependacies file."
-  homepage "https://github.com/AhmedOsman00/arrow-script"
-  url "https://github.com/AhmedOsman00/repo/releases/download/tag/arrow"
-  sha256 "sha256"
-  version "version"
+  desc "Swift-based command-line tool for generating dependency extension"
+  homepage "https://github.com/AhmedOsman00/arrow-generator"
+
+  # These values will be replaced by brew bump-formula-pr
+  url "https://example.com/arrow"
+  version "0.0.0"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   def install
     bin.install "arrow"
+  end
+
+  test do
+    system "#{bin}/arrow", "--help"
   end
 end
