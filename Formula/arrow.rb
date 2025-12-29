bb073e515af9078858d600c6a2011b5cc78bbfca
Ahmed Osman <eng.ahmedosman00@gmail.com>
2025-12-29 15:08:37 +0100

    Fix version format in arrow.rb formula

--- Formula/arrow.rb
+++ Formula/arrow.rb
@@ -4,7 +4,7 @@ class Arrow < Formula
 
   # These values will be replaced by brew bump-formula-pr
   url "https://github.com/AhmedOsman00/arrow-generator/releases/download/v1.0.0/arrow"
-  version "v1.0.0"
+  version "1.0.0"
   sha256 "d84cb9e7761a5bc63ce135e9b1b043539eb2e6bd71fdb6055f1d426e14b21912"
 
   def install
