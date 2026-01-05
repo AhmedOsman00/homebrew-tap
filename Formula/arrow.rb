8c21c689aa33c2c1ab584a756df6d23df4e9237c
Ahmed Osman <eng.ahmedosman00@gmail.com>
2026-01-05 16:06:17 +0100

    arrow 1.0.2 (#3)
    Co-authored-by: Anka <runner@sjc20-cw715-86b793d5-28ce-430a-ba7d-f7db5977bae4-0A5F00054FF4.local>

--- Formula/arrow.rb
+++ Formula/arrow.rb
@@ -3,9 +3,9 @@ class Arrow < Formula
   homepage "https://github.com/AhmedOsman00/arrow-generator"
 
   # These values will be replaced by brew bump-formula-pr
-  url "https://github.com/AhmedOsman00/arrow-generator/releases/download/v1.0.1/arrow"
-  version "1.0.1"
-  sha256 "db1fd6b0cd845e302db7319d3905f03404035735f55638021bf3b5d21389b858"
+  url "https://github.com/AhmedOsman00/arrow-generator/releases/download/v1.0.2/arrow"
+  version "1.0.2"
+  sha256 "a609c182cfe4c26b83730351d2146c34abce918a59cbc80ac79fa16d0a7dbb15"
 
   def install
     bin.install "arrow"
